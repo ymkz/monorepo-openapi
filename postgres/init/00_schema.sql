9d2a67f6a93f9fb08902c69c52f750173a698bdf
ymkz <ymkz@users.noreply.github.com>
2024-07-18 23:22:57 +0900

    chore: setup postgres wip

---
+++ postgres/init/00_schema.sql
@@ -0,0 +1,30 @@
+-- CREATE DATABASE local_db;
+
+-- \c local_db;
+
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE todos (
+  id serial NOT NULL,
+	title varchar(40) NOT NULL,
+  completed_at timestamp NULL,
+	created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at timestamp NULL,
+	deleted_at timestamp NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE todos_id_seq START 1;
+
+CREATE OR REPLACE FUNCTION update_by_current_timestamp() RETURNS TRIGGER AS
+$$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER update_todos_updated_at
+  BEFORE UPDATE ON todos
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_by_current_timestamp();
